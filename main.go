3ef2b51f651929dec70ebd8332ee7f853ac6527b
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2024-11-27 20:51:44 +0000

    feat: create client and hub structs
    Signed-off-by: Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>

--- main.go
+++ main.go
@@ -9,11 +9,18 @@ import (
 func main() {
 	router := gin.Default()
 
+	// Create a new hub
+	hub := NewHub()
+	// Run hub in a goroutine
+	// go hub.run()
+
+	// Sever entry point
 	router.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{
 			"message": "Hello World",
 		})
 	})
+
 
 	// Run the server
 	router.Run(":8080")
