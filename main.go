64b71ac18c6eb3eecc97710165efad8d83f57117
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2024-11-27 20:45:47 +0000

    chore: start a server
    Signed-off-by: Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "Hello World",
+		})
+	})
+
+	// Run the server
+	router.Run(":8080")
+}
